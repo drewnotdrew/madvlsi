27740fbfa7e0508fca2e6f2ccf79a41f81a43bdf
drewnotdrew <silverbird337@gmail.com>
2025-10-12 16:02:23 -0400

    util/xschem_plot: update for csrl

--- util/xschem_plot.py
+++ util/xschem_plot.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 # plot colors
 light_sea_green = "#038880"
@@ -26,16 +27,50 @@ def plot(file: Path):
     
   data["time"] = [_time / 1e-9 for _time in data["time"]]
 
-  plt.figure(figsize=(12,6))
-  plt.title("AND Gate Transient Response")
+  figure_0 = plt.figure(figsize=(12,20))
+  
+  # sim_corners = [
+  #   Patch(facecolor=taupe_gray, edgecolor=taupe_gray, label="TT"),
+  #   Patch(facecolor=bittersweet, edgecolor=bittersweet, label="FF"),
+  #   # Patch(facecolor=taupe_gray, edgecolor=taupe_gray, label="TT"),
+  # ]
+  
+  # figure_0.legend(handles=sim_corners, loc="upper right", bbox_to_anchor=(0.9, 0.5))
+
+  plt.axis('off')
+  plt.legend(["TT"])
+  plt.title("Four-Bit Shift Register Transient Response, SS")
   plt.xlabel("Time (ns)")
   plt.ylabel("Voltage")
-  plt.plot(data["time"], data["v(A)"], color=light_sea_green, linestyle="--", linewidth=2)
-  plt.plot(data["time"], data["v(B)"], color=bittersweet, linestyle="--", linewidth=2)
-  plt.plot(data["time"], data["v(Y)"], color=black, linewidth=2)
-  plt.minorticks_on()
+  plt.subplot(11, 1, 1)
   plt.grid(which="minor", linestyle="--", linewidth=0.5)
-  plt.legend(["v(A)", "v(B)", "v(Y)"])
+  plt.minorticks_on()
+  plt.plot(data["time"], data["v(clk)"], color=black, linewidth=2)
+  plt.legend(["v(clk)"], loc="right")
+  plt.subplot(11, 1, 2)
+  plt.grid(which="minor", linestyle="--", linewidth=0.5)
+  plt.minorticks_on()
+  plt.plot(data["time"], data["v(D)"], color=black, linewidth=2)
+  plt.legend(["v(D)"], loc="right")
+  plt.subplot(11, 1, 3)
+  plt.grid(which="minor", linestyle="--", linewidth=0.5)
+  plt.minorticks_on()
+  plt.plot(data["time"], data["v(Dn)"], color=black, linewidth=2)
+  plt.legend(["v(Dn)"], loc="right")
+
+  for index in range(0,4):
+    plt.subplot(11, 1, 4 + index*2)
+    plt.grid(which="minor", linestyle="--", linewidth=0.5)
+    plt.minorticks_on()
+    plt.plot(data["time"], data[f"v(Q{index})"], color=black, linewidth=2)
+    plt.legend([f"v(Q{index})"], loc="right")
+
+    plt.subplot(11, 1, 5 + index*2)
+    plt.grid(which="minor", linestyle="--", linewidth=0.5)
+    plt.minorticks_on()
+    plt.plot(data["time"], data[f"v(Qn{index})"], color=black, linewidth=2)
+    plt.legend([f"v(Qn{index})"], loc="right")
+
   plt.savefig("plot.png")
 
 if __name__ == "__main__":
